dedde6f480d0e3c29b844300cf3a417a61bafdb0
rvbear <rvbear2000@gmail.com>
2024-02-25 18:07:52 +0900

    [feat] 회원가입 간단 구현 완료

--- src/main/java/com/example/backend/domain/users/controller/UserController.java
+++ src/main/java/com/example/backend/domain/users/controller/UserController.java
@@ -1,11 +1,23 @@
 package com.example.backend.domain.users.controller;
 
+import com.example.backend.domain.users.dto.UserSignUpDto;
 import com.example.backend.domain.users.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+
+    @Operation(summary = "회원가입", description = "회원가입 성공 여부를 반환합니다.")
+    @PostMapping("/sign_up")
+    public String signUp(@RequestBody UserSignUpDto userSignUpDto) throws Exception {
+        userService.signUp(userSignUpDto);
+
+        return "회원가입 성공";
+    }
 }
--- src/main/java/com/example/backend/domain/users/service/UserService.java
+++ src/main/java/com/example/backend/domain/users/service/UserService.java
@@ -1,5 +1,7 @@
 package com.example.backend.domain.users.service;
 
+import com.example.backend.domain.users.dto.UserSignUpDto;
+import com.example.backend.domain.users.entity.User;
 import com.example.backend.domain.users.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,4 +10,24 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+
+    public void signUp(UserSignUpDto userSignUpDto) throws Exception {
+        if(userRepository.findByEmail(userSignUpDto.getEmail()).isPresent()) {
+            throw new Exception("이미 존재하는 이메일입니다.");
+        }
+
+        if(userRepository.findByUsername(userSignUpDto.getUsername()).isPresent()) {
+            throw new Exception("이미 존재하는 닉네임입니다.");
+        }
+
+        if(!userSignUpDto.getPassword().equals(userSignUpDto.getRe_password())) {
+            throw new Exception("비밀번호를 다시 확인해주세요.");
+        }
+
+        User user = User.builder()
+                .userSignUpDto(userSignUpDto)
+                .build();
+
+        userRepository.save(user);
+    }
 }
