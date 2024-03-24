5471bb6bd49fdceb82146c76ed7249e20bc65926
rvbear <rvbear2000@gmail.com>
2024-03-25 01:02:43 +0900

    [feat] test data 추가

--- src/main/java/com/example/backend/DataLoader.java
+++ src/main/java/com/example/backend/DataLoader.java
@@ -1,9 +1,11 @@
 package com.example.backend;
 
 import com.example.backend.model.entity.CategoryEntity;
+import com.example.backend.model.entity.DiaryEntity;
 import com.example.backend.model.entity.TodoEntity;
 import com.example.backend.model.entity.UserEntity;
 import com.example.backend.repository.CategoryRepository;
+import com.example.backend.repository.DiaryRepository;
 import com.example.backend.repository.TodoRepository;
 import com.example.backend.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +18,7 @@ public class DataLoader implements CommandLineRunner {
     private final TodoRepository todoRepository;
     private final CategoryRepository categoryRepository;
     private final UserRepository userRepository;
+    private final DiaryRepository diaryRepository;
 
     @Override
     public void run(String... args) {
@@ -25,5 +28,7 @@ public class DataLoader implements CommandLineRunner {
 
         todoRepository.save(new TodoEntity("string1", Long.parseLong("1"), Long.parseLong("1"), "2024-02-24"));
         todoRepository.save(new TodoEntity("string2", Long.parseLong("1"), Long.parseLong("1"), "2024-02-24"));
+
+        diaryRepository.save(new DiaryEntity("Good", "String", "2024-02-24", Long.parseLong("1")));
     }
 }
